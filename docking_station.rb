6ef3f6c0cd5908adb9834fc2c7501427381f07b6
Jeromee <jerome791@hotmail.co.uk>
2020-04-27 16:32:12 +0100

    release bike test started

--- docking_station.rb
+++ docking_station.rb
@@ -1,4 +1,8 @@
 class DockingStation
   def initialize
   end
+
+  def release_bike
+    return true
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require "../docking_station.rb"
 
-describe "Docking station should release bike" do
+describe "Docking station is an instance" do
   it "creates new instance of docking station" do
     expect(DockingStation.new).to be_instance_of DockingStation
+  end
+
+  it "Docking station should release bike" do
+      docking_1 = DockingStation.new
+      expect(docking_1.release_bike).to(eq(true))
   end
 end
