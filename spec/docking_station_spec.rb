a2a8e4dcffc073b9fd30dd8be625b045ffb5a6c1
Adewunmi Awolesi <adewunmi.awolesi@beis.gov.uk>
2020-04-29 15:34:26 +0100

    Chapter 14 done

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,10 +7,15 @@ describe DockingStation do
   it "responds to release_bike" do
     expect(subject).to respond_to :release_bike
   end
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+  it 'releases a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
   end
+  it 'raises an error when there are no bikes available' do 
+    expect {subject.release_bike }.to raise_error 'No bikes available'
+end
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
@@ -20,7 +25,14 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+  describe '#dock' do
+  it 'raises an error when full' do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+  end
 end
+end
+end 
 
 
 
