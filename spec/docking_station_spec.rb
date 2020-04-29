5de91be2af402ee45a081501f1b63373c088f559
Anna <anna.voitik@gmail.com>
2020-04-29 13:55:25 +0100

    Steps 10 and 11, we can now dock a bike and see which bike is docked

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,5 +11,14 @@ describe DockingStation do
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+  it 'docks something' do
+    bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
+  end
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end 
 end
